cf396476e1ba56ed802ac24fc1c57a0fd092c3d0
Fariz Muradov <aze2201@gmail.com>
2023-08-01 15:59:05 +0200

    added xample  rule.lua

---
+++ nginx/mlib/rule.lua
@@ -0,0 +1,33 @@
+function on_fly (url, body, header)
+            local stream_body = 'no content'
+            -- library for decompress body
+            local zlib = require "zlib"
+            local brotlidec = require "brotli.decoder"
+            -- if no header define something to avoid nil value
+            if header then
+                print (header)
+            else
+                header='xxx'
+            end
+            -- need to decompress if no Content-Encoding specified
+            -- if header then
+                00
+            if string.find(body, "DOCTYPE") then
+                if string.find(header, "gzip") then                   
+                    local stream = zlib.inflate()
+                    local stream_body = stream(body)
+                elseif string.find(header, "br") then
+                    local decoder = brotlidec:new()
+                    local stream_body, err = decoder:decompress(body)
+                else
+                        stream_body = body
+                end
+            end
+            -- else
+            --    print ('no header provided for this URL ',url)
+            -- end
+
+            -- here you can add any operation. Like printing, mailing etc.
+            print(stream_body)
+
+end
